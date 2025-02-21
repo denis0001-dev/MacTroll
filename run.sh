31762173dc53fcfdafd44a61bf1b36ef136ccb7c
denis0001-dev <denis0001.dev@ya.ru>
2025-02-21 16:12:52 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd /tmp
+mkdir troll
+cd troll
+curl -O https://github.com/denis0001-dev/MacTroll/raw/refs/heads/main/launchprogresswindow
+curl -O https://github.com/denis0001-dev/MacTroll/raw/refs/heads/main/troll.sh
+chmod +x troll.sh
+
+cleanup() {
+  rm -rf /tmp/troll
+}
+
+read seconds
+if ! [[ $seconds =~ ^-?[0-9]+$ ]]; then
+  echo "Error: invalid integer"
+  cleanup
+  exit 1
+fi
+./troll.sh $seconds
+cleanup
